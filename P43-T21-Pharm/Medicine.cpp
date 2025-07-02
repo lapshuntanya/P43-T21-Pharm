e6718c89472a716d860e27685fa63a357b3a29bd
lapshun_t_s <lapshun.tatiana@gmail.com>
2025-07-02 17:26:14 +0300

    cpp

--- P43-T21-Pharm/Medicine.cpp
+++ P43-T21-Pharm/Medicine.cpp
@@ -1,1 +1,44 @@
 #include "Medicine.h"
+
+Medicine::Medicine()
+{
+}
+
+Medicine::Medicine(const char* _title, const char* _type, float _price)
+{
+}
+
+Medicine::~Medicine()
+{
+}
+
+void Medicine::setTitle(const char* _title)
+{
+}
+
+void Medicine::setType(const char* _type)
+{
+}
+
+void Medicine::setPrice(float _price)
+{
+}
+
+const char* Medicine::getTitle() const
+{
+	return nullptr;
+}
+
+const char* Medicine::getType() const
+{
+	return nullptr;
+}
+
+float Medicine::getPrice() const
+{
+	return 0.0f;
+}
+
+void Medicine::showInfo() const
+{
+}
